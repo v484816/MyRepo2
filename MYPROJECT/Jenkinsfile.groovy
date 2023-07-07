624f1af1216111de30b2a56ec16ee0773cc24639
vibhanshmanoj.chavh <vibhanshmanoj.chavh@LP1-AP-52179647.PROD.HCLPNP.COM>
2023-07-07 16:37:11 +0530

    6 commit

--- MYPROJECT/Jenkinsfile.groovy
+++ MYPROJECT/Jenkinsfile.groovy
@@ -1,30 +1,25 @@
 pipeline {
     agent any
-stages{
-    stage('Functional Test - HCL OneTest UI')
 
- 
+    stages {
+        stage('Build') {
+            steps {
+                echo 'Building..'
+                // Get some code from a GitHub repository
+                git url: 'https://github.com/v484816/MyRepo2', branch: 'main'
+                
+            }
+        }
+        stage('Functional Test - HCL OneTest Performance') {
+            steps {
+                step([$class: 'PerformanceTesterProductHelper', configfile: 'C:\\Users\\vibhanshmanoj.chavh\\HCL\\hclonetest\\New folder\\MYPROJECT\\my-config.txt', duration: '', exportlog: '', exportstatreportlist: '', exportstats: '', exportstatsformat: '', exportstatshtml: '', history: '', imsharedloc: '', labels: '', name: 'MYPROJECT', overridermlabels: '', overwrite: 'true', project: '', publish: '', publish_for: '', publishreports: '', rate: '', results: '', suite: '', swapdatasets: '', usercomments: '', users: '', varfile: '', vmargs: '', workspace: ''])
 
-            {
-
- 
-
-              steps
-
- 
-
-                {
-
- 
-
-                   step([$class: 'PerformanceTesterProductHelper', configfile: 'C:\\Users\\vibhanshmanoj.chavh\\HCL\\hclonetest\\New folder\\MYPROJECT\\my-config.txt', duration: '', exportlog: '', exportstatreportlist: '', exportstats: '', exportstatsformat: '', exportstatshtml: '', history: '', imsharedloc: '', labels: '', name: 'MYPROJECT', overridermlabels: '', overwrite: 'true', project: '', publish: '', publish_for: '', publishreports: '', rate: '', results: '', suite: '', swapdatasets: '', usercomments: '', users: '', varfile: '', vmargs: '', workspace: ''])
-
- 
-
-                }
-
- 
-
-            }      
+            }
+        }
+        stage('Deploy') {
+            steps {
+                echo 'Deploying....'
+            }
+        }
     }
 }
